6bf33c510b7eb83a83ad9ca3eea0e7c78372c65b
Ozan <ozan28061@gmail.com>
2022-12-07 20:20:01 +0300

    first chat commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  socket.on("username", (username) => {
+    io.emit("username", username);
+    console.log("username: ", username);
+  });
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+    console.log("Message: ", msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
