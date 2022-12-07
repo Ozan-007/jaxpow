078e8640d26d369ed28b7654fad42d417954059f
Ozan <ozan28061@gmail.com>
2022-12-08 00:24:50 +0300

    given both ports to them

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ io.on("connection", (socket) => {
 //   console.log("listening on *:3000");
 // });
 
-server.listen(process.env.PORT || 3000, () => {
+server.listen(3000, () => {
   console.log("listening on *:3000");
 });
