01e3ba804261f683ce163b406f6630f1c1b7666f
Ozan <ozan28061@gmail.com>
2022-12-08 00:02:01 +0300

    app.listen added instead of server.listen

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("listening on *:3000");
 });
+
+// server.listen(process.env.PORT || 3000, () => {
+//   console.log("listening on *:3000");
+// });
