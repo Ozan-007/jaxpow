a23bb0c56529bc6ed321c7af716c95dd4bc24c9e
Ozan <ozan28061@gmail.com>
2022-12-07 20:40:40 +0300

    io() url added

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
+server.listen("https://clinquant-macaron-7cbf66.netlify.app/", () => {
   console.log("listening on *:3000");
 });
