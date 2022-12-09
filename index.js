8216704bed9ee10090a9bb58c978e8bc4ec3091a
Ozan <ozan28061@gmail.com>
2022-12-09 14:34:04 +0300

    io("/") added

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 const express = require("express");
 const app = express();
 const http = require("http");
-const cors = require("cors");
-app.use(cors());
-
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 
-const io = new Server(server, {
-  cors: {
-    origin: "https://clinquant-macaron-7cbf66.netlify.app/",
-    methods: ["GET", "POST"],
-  },
-});
+const io = new Server(server);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
